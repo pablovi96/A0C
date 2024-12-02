94659b79eee3129c7148cd2f70e304c06f54d3fc
Pablo Viñolas <p.vinolas@biele.com>
2024-12-02 13:04:27 +0100

    good

--- day2.py
+++ day2.py
@@ -10,29 +10,37 @@ for linea in datos:
     good=True
     safe=0
     while (i<len(linea)-1):
-        if(linea[0]-linea[1]>0):
-            if (linea[i]-linea[i+1]>3 or linea[i]-linea[i+1]<=0):          
+        if(linea[0]>linea[1]): #dsescendente
+            if (linea[i]-linea[i+1]>3 or linea[i]-linea[i+1]<=0):
+                linea.pop(i+1)
+                i=0
                 safe=safe+1
                 if safe==2:
                     good=False
                     break
-
-        elif(linea[0]-linea[1]<0):
+                
+        elif(linea[0]<linea[1]): #asciende
             if (linea[i]-linea[i+1]<-3 or linea[i]-linea[i+1]>=0):
                 safe=safe+1
-
+                linea.pop(i+1)
+                i=0
                 if safe==2:
                     good=False
                     break
         else:
             safe=safe+1
-            if linea[1]-linea[2]<0:
-                linea[0]=0
-            else:
+            if linea[1]<linea[2]:
+                linea[0]=-11000
+            elif linea[1]>linea[2]:
                 linea[0]=100000
+            else:
+                good=False
+                break
     
 
         i=i+1
+
     if good==True:
         cont=cont+1
+    print(good)
 print(cont)
